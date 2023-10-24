d8cb22b512d46ddf3b4d491d5ecdfc13f002a57a
RaihanSulthon <raihansy@student.telkomuniversity.ac.id>
2023-10-24 13:16:17 +0700

    Tambah Edit Profile, Button Done Edit, Edit gambar profile

--- frontend/src/Components/Profile.jsx
+++ frontend/src/Components/Profile.jsx
@@ -1,23 +1,63 @@
-import React from "react";
-import Ray from "../img/Raychan.jpg";
+import React, { useState } from "react";
+import user from "../img/user.png";
+import EditProfile from "../img/editing.png";
 
 const Profile = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [userImage, setUserImage] = useState(user);
   const User = localStorage.getItem("User");
+
+  const handleEditProfile = () => {
+    setIsEditing(!isEditing);
+  };
+
+  const handleDoneEditing = () => {
+    setIsEditing(false);
+  };
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setUserImage(e.target.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div className="px-44">
-      <div className="relative h-60 rounded-b flex justify-center bg-red-600">
-        <div className="absolute -bottom-10">
+    <div className="px-20">
+      <div className="relative h-60 rounded-b-3xl flex justify-center bg-red-600">
+        <div className="absolute -bottom-20">
           <img
-            src={Ray}
+            src={userImage}
             className="object-cover border-4 border-white w-40 h-40 rounded-full"
             alt="cover"
           />
         </div>
       </div>
-      <div className="text-center mt-16 text-3xl font-bold text-black">
-        Profile
+      <div className="text-center pt-20 text-3xl font-bold text-black">
+        <div className="flex items-center justify-between">
+          Profile
+          <label htmlFor="file" className="cursor-pointer">
+            <img
+              src={EditProfile}
+              className="w-5 h-auto cursor-pointer"
+              alt="edit"
+              onClick={handleEditProfile}
+            />
+          </label>
+          <input
+            type="file"
+            id="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            style={{ display: "none" }}
+          />
+        </div>
       </div>
-      <div className="border border-gray-500 mt-5 mb-2 border-opacity-1"></div>
+      <div className="border border-gray-500 mt-3 mb-2 border-opacity-1"></div>
 
       {/* Label Biodata */}
       <form className="flex flex-col sm:flex-row gap-3 sm:gap-4" action="">
@@ -30,10 +70,12 @@ const Profile = () => {
             </label>
             <input
               placeholder=""
-              className="p-1 sm:p-2 text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
+              className={`p-1 sm:p-2 ${
+                isEditing ? "bg-gray-200" : ""
+              } text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg`}
               type="text"
-              value={"Fasya Raihan Maulana"}
               id="nama"
+              readOnly={!isEditing}
             />
           </div>
           <div className="flex flex-col mt-4">
@@ -44,10 +86,12 @@ const Profile = () => {
             </label>
             <input
               placeholder=""
-              className="p-1 sm:p-2 text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
+              className={`p-1 sm:p-2 ${
+                isEditing ? "bg-gray-200" : ""
+              } text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg`}
               type="text"
-              value={"1302210016"}
               id="NIM"
+              readOnly={!isEditing}
             />
           </div>
           <div className="flex flex-col mt-4">
@@ -58,10 +102,12 @@ const Profile = () => {
             </label>
             <input
               placeholder=""
-              className="p-1 sm:p-2 text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
+              className={`p-1 sm:p-2 ${
+                isEditing ? "bg-gray-200" : ""
+              } text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg`}
               type="text"
-              value={"Laki-Laki"}
               id="Gender"
+              readOnly={!isEditing}
             />
           </div>
         </div>
@@ -74,10 +120,12 @@ const Profile = () => {
             </label>
             <input
               placeholder=""
-              className="p-1 sm:p-2 text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
+              className={`p-1 sm:p-2 ${
+                isEditing ? "bg-gray-200" : ""
+              } text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg`}
               type="text"
-              value={"081190843272"}
               id="Nomor HP"
+              readOnly={!isEditing}
             />
           </div>
           <div className="flex flex-col mt-4">
@@ -88,10 +136,12 @@ const Profile = () => {
             </label>
             <input
               placeholder=""
-              className="p-1 sm:p-2 text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
+              className={`p-1 sm:p-2 ${
+                isEditing ? "bg-gray-200" : ""
+              } text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg`}
               type="text"
-              value={"Fakultas Informatika"}
               id="Fakultas"
+              readOnly={!isEditing}
             />
           </div>
           <div className="flex flex-col mt-4">
@@ -102,14 +152,49 @@ const Profile = () => {
             </label>
             <input
               placeholder=""
-              className="p-1 sm:p-2 text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
+              className={`p-1 sm:p-2 ${
+                isEditing ? "bg-gray-200" : ""
+              } text-xs h-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg`}
               type="text"
-              value={"S1 Rekayasa Perangkat Lunak"}
               id="Jurusan"
+              readOnly={!isEditing}
             />
           </div>
         </div>
       </form>
+      <div className="mt-4 flex w-1/2">
+        {isEditing && (
+          <button
+            className="
+            mr-3
+            items-center 
+            justify-center 
+            rounded-md 
+            border border-transparent 
+            bg-green-300
+            px-8 py-3 
+            text-base 
+            font-medium text-white
+            hover:bg-green-500 "
+            onClick={handleDoneEditing}>
+            Done
+          </button>
+        )}
+        <button
+          className="
+          items-center 
+          justify-center 
+          rounded-md 
+          border border-transparent 
+          bg-red-300
+          px-8 py-3 
+          text-base 
+          font-medium text-white
+          hover:bg-red-500"
+          onClick={handleDoneEditing}>
+          Back
+        </button>
+      </div>
     </div>
   );
 };
