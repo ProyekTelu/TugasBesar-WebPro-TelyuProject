f2b98f214a12dbc8ec5e4d817435d17d4b538686
rezadhied <rezadhied@student.telkomuniversity.ac.id>
2023-11-05 21:58:05 +0700

    update modal

--- frontend/src/Components/PageComponent/Lecturer/CreateProjectModal.jsx
+++ frontend/src/Components/PageComponent/Lecturer/CreateProjectModal.jsx
@@ -52,7 +52,7 @@ function CreateProjectModal({ isOpen, closeModal }) {
   ]);
   return (
     <div className="w-full ">
-      <div className="justify-center modal-container w-3/5 mx-auto">
+      <div className="justify-center modal-container w-3/5 mx-auto transition-transform transform hover:scale-105">
         <div className="w-full relative shadow-lg p-12 my-4 rounded-3xl justify-center h-full overflow-y-auto bg-whiteAlternative flex flex-col">
           <button
             onClick={closeModal}
@@ -63,9 +63,12 @@ function CreateProjectModal({ isOpen, closeModal }) {
           <h1 className="text-center text-xl xs:text-lg sm:text-2xl md:text-4xl md:my-4 font-bold">
             Create Project
           </h1>
-          <form onSubmit={handleSubmit}>
-            <div className="flex gap-5 flex-row w-full m-auto justify-center">
-              <div className="w-full">
+          <form
+            className="flex flex-col sm:flex-row gap-4 xs:gap-6 md:gap-8 xl:gap-10"
+            onSubmit={handleSubmit}
+          >
+            <div className="w-full xs:w-1/2">
+              <div className="flex flex-col mt-4">
                 <label className="font-medium text-xs text-textGray md:text-base after:content-['*'] after:ml-0.5 after:text-red-500 block">
                   Project Title{" "}
                   {projectTitle.length < 3 && projectTitle != "" && (
@@ -85,29 +88,7 @@ function CreateProjectModal({ isOpen, closeModal }) {
                 </div>
               </div>
 
-              <div className="w-full">
-                <label className="font-medium text-xs text-textGray md:text-base after:content-['*'] after:ml-0.5 after:text-red-500 block">
-                  Group Chat Link{" "}
-                  {!groupChatLink.includes(".com") && groupChatLink !== "" && (
-                    <span className="text-primary font-normal">
-                      Fill the correct link.
-                    </span>
-                  )}
-                </label>
-                <div className="">
-                  <input
-                    type="text"
-                    value={groupChatLink}
-                    onChange={(e) => setGroupChatLink(e.target.value)}
-                    className="p-1 sm:p-2 text-xs h-full w-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
-                    placeholder="http://www.example.com"
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="flex gap-5 flex-row pt-6 w-full justify-center">
-              <div className="w-full">
+              <div className="flex flex-col mt-4">
                 <label className="font-medium text-xs text-textGray md:text-base after:content-['*'] after:ml-0.5 after:text-red-500 block">
                   Description{" "}
                   {description.length < 12 && description !== "" && (
@@ -129,29 +110,7 @@ function CreateProjectModal({ isOpen, closeModal }) {
                 </div>
               </div>
 
-              <div className="w-full">
-                <label className="font-medium text-xs text-textGray md:text-base after:content-['*'] after:ml-0.5 after:text-red-500 block ">
-                  Maximum Member{" "}
-                  {maxMembers < 1 && maxMembers !== "" && (
-                    <span className="text-primary font-normal">
-                      At least 1 member.
-                    </span>
-                  )}
-                </label>
-                <div className="control">
-                  <input
-                    type="number"
-                    className="p-1 sm:p-2 text-xs h-full w-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
-                    placeholder="0"
-                    value={maxMembers}
-                    onChange={(e) => setMaxMembers(e.target.value)}
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div className="flex gap-5 flex-row pt-6 w-full justify-center">
-              <div className="w-1/2">
+              <div className="flex flex-col mt-4">
                 <label
                   className="font-medium text-xs text-textGray md:text-base after:content-['*'] after:ml-0.5 after:text-red-500 block"
                   htmlFor=""
@@ -170,58 +129,101 @@ function CreateProjectModal({ isOpen, closeModal }) {
                   <option>Teknik Elektro</option>
                 </select>
               </div>
+            </div>
 
-              <div className="flex flex-row justify-between w-1/2">
+            <div className="w-full xs:w-1/2">
+              <div className="flex flex-col mt-4">
+                <label className="font-medium text-xs text-textGray md:text-base after:content-['*'] after:ml-0.5 after:text-red-500 block">
+                  Group Chat Link{" "}
+                  {!groupChatLink.includes(".com") && groupChatLink !== "" && (
+                    <span className="text-primary font-normal">
+                      Fill the correct link.
+                    </span>
+                  )}
+                </label>
                 <div className="">
-                  <label className="font-medium text-xs text-textGray md:text-base after:content-['*'] after:ml-0.5 after:text-red-500 block">
-                    Project Start
-                  </label>
-                  <div className="">
-                    <DatePicker
-                      selected={startDate}
-                      onChange={(date) => {
-                        setStartDate(date);
-                        if (date > endDate) {
-                          setEndDate(date);
-                        }
-                      }}
-                      className="p-1 sm:p-2 text-xs h-full w-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
-                      placeholderText="Set Date"
-                    />
-                  </div>
+                  <input
+                    type="text"
+                    value={groupChatLink}
+                    onChange={(e) => setGroupChatLink(e.target.value)}
+                    className="p-1 sm:p-2 text-xs h-full w-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
+                    placeholder="http://www.example.com"
+                  />
                 </div>
+              </div>
 
+              <div className="flex flex-col mt-4">
+                <label className="font-medium text-xs text-textGray md:text-base after:content-['*'] after:ml-0.5 after:text-red-500 block ">
+                  Maximum Member{" "}
+                  {maxMembers < 1 && maxMembers !== "" && (
+                    <span className="text-primary font-normal">
+                      At least 1 member.
+                    </span>
+                  )}
+                </label>
+                <div className="control">
+                  <input
+                    type="number"
+                    className="p-1 sm:p-2 text-xs h-full w-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
+                    placeholder="0"
+                    value={maxMembers}
+                    onChange={(e) => setMaxMembers(e.target.value)}
+                  />
+                </div>
+              </div>
+              
+
+            <div className="flex flex-row justify-between mt-4">
+              <div className="">
+                <label className="font-medium text-xs text-textGray md:text-base after:content-['*'] after:ml-0.5 after:text-red-500 block">
+                  Project Start
+                </label>
                 <div className="">
-                  <label className="font-medium text-xs text-textGray md:text-base after:content-['*'] after:ml-0.5 after:text-red-500 block">
-                    Project End
-                  </label>
-                  <div className="">
-                    <DatePicker
-                      selected={endDate}
-                      onChange={(date) => setEndDate(date)}
-                      minDate={startDate}
-                      className="p-1 sm:p-2 text-xs h-full w-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
-                      placeholderText="Set Date"
-                    />
-                  </div>
+                  <DatePicker
+                    selected={startDate}
+                    onChange={(date) => {
+                      setStartDate(date);
+                      if (date > endDate) {
+                        setEndDate(date);
+                      }
+                    }}
+                    className="p-1 sm:p-2 text-xs h-full w-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
+                    placeholderText="Set Date"
+                  />
+                </div>
+              </div>
+
+              <div className="">
+                <label className="font-medium text-xs text-textGray md:text-base after:content-['*'] after:ml-0.5 after:text-red-500 block">
+                  Project End
+                </label>
+                <div className="">
+                  <DatePicker
+                    selected={endDate}
+                    onChange={(date) => setEndDate(date)}
+                    minDate={startDate}
+                    className="p-1 sm:p-2 text-xs h-full w-full md:text-base focus:outline-black border-textGray border-[0.5px] md:border-[1px] border-solid rounded-md md:rounded-lg"
+                    placeholderText="Set Date"
+                  />
                 </div>
               </div>
             </div>
-
-            <div className="w-full flex justify-center mt-10 pt-0 xs:pt-2">
-              <button
-                type="submit"
-                className={`"text-secondary text-white w-1/4 py-1 block sm:py-3 md:text-lg text-xs px-2 md:px-5 rounded-md md:rounded-lg" ${
-                  !isInputComplete
-                    ? "bg-black cursor-not-allowed"
-                    : " bg-primary hover:bg-brightPrimary cursor-pointer"
-                }`}
-                disabled={!isInputComplete}
-              >
-                Create
-              </button>
             </div>
           </form>
+
+          <div className="w-full flex justify-center mt-10 pt-0 xs:pt-2">
+            <button
+              type="submit"
+              className={`" text-white py-2 sm:py-3 text-xs md:text-lg px-8 md:px-5 rounded-md md:rounded-lg block" ${
+                !isInputComplete
+                  ? "bg-black cursor-not-allowed"
+                  : " bg-secondary hover:bg-brightPrimary cursor-pointer"
+              }`}
+              disabled={!isInputComplete}
+            >
+              Create
+            </button>
+          </div>
         </div>
       </div>
     </div>
