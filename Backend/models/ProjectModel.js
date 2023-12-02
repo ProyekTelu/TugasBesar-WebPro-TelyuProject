2cf504275ac92abcf01e1ed4c45e8484031179c9
mzaky <mzakyf5503@gmail.com>
2023-12-02 13:20:06 +0700

    update request

--- Backend/models/ProjectModel.js
+++ Backend/models/ProjectModel.js
@@ -1,6 +1,7 @@
 import { Sequelize } from "sequelize";
 import db from "../config/Database.js";
 import User from "./UserModel.js";
+import Request from "./RequestModel.js";
 
 const { DataTypes } = Sequelize;
 
@@ -60,6 +61,11 @@ Project.belongsTo(User, {
   foreignKey: "projectOwnerID",
   as: "projectOwner",
   targetKey: "userID",
+});
+
+Project.hasMany(Request, {
+  foreignKey: "projectID",
+  sourceKey: "projectID",
 });
 
 export default Project;
--- frontend/src/Components/PageComponent/Lecturer/Requested.jsx
+++ frontend/src/Components/PageComponent/Lecturer/Requested.jsx
@@ -21,8 +21,10 @@ const Requested = () => {
   return (
     <div className="p-4 md:p-12 overflow-y-auto flex justify-center w-full ">
       <div className="w-full mx-auto grid md:grid-cols-1 gap-4 xs:grid-cols-1  xs:gap-4 xss:grid-cols-1 ">
-        <div className="flex justify-between gap-5 flex-col  md:flex-row">
-          <label className=" font-bold text-2xl">REQUESTED</label>
+        <div className="flex justify-between gap-5 flex-col md:flex-row">
+          <label className="px-4  text-xl md:text-2xl text-primary font-bold text-start">
+            Requested
+          </label>
 
           <input
             className=" transition-transform transform hover:scale-105  flex space-x-4 placeholder:italic align-middle w-auto
@@ -66,7 +68,15 @@ const Requested = () => {
                   <span className="font-semibold text-md md:text-lg">
                     Message
                   </span>{" "}
-                  : izin kan saya masuk
+                  : Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Possimus nisi eum voluptas ea error velit quia aperiam
+                  asperiores reiciendis ut quaerat sunt harum, delectus,
+                  accusantium molestias eligendi eos obcaecati. Repellendus!
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatibus architecto consequuntur, sunt aspernatur vel
+                  maxime accusamus eos magni recusandae adipisci, sed, quidem
+                  voluptatum aliquam officiis veniam esse placeat reiciendis
+                  quos.
                 </label>
               </div>
               <div className=" flex flex-row md:flex-col justify-center gap-4 my-2">
@@ -171,120 +181,19 @@ const Requested = () => {
                   </span>{" "}
                   Webpro
                 </label>
-                <label className="mx-auto md:mx-8 text-md md:text-lg">
+                <label className="mx-auto md:mx-8 text-md md:text-lg line-clamp-2">
                   <span className="font-semibold text-md md:text-lg">
                     Message
-                  </span>{" "}
-                  : izin kan saya masuk
-                </label>
-              </div>
-              <div className=" flex flex-row md:flex-col justify-center gap-4 my-2">
-                <button className="  hover:bg-secondaryAlternative  transition-transform transform hover:scale-105 active:scale-95 p-4 bg-secondary  w-24 md:w-32 h-14   rounded-md text-white">
-                  Approve
-                </button>
-                <button className="  hover:bg-primaryAlternative transition-transform transform hover:scale-105 active:scale-95  p-4 bg-primary w-24  h-14  md:w-32  rounded-md text-white">
-                  Decline
-                </button>
-              </div>
-            </div>
-            <div className="flex justify-center ">
-              <label
-                onClick={toggleModal}
-                className="transition-transform transform hover:scale-105 mt-4 text-cyan-400 cursor-pointer active:scale-95"
-              >
-                View {"Naufal"} Requested
-              </label>
-            </div>
-          </div>
-
-          {modal && (
-            <div
-              onClick={toggleModal}
-              className=" fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-opacity-5 backdrop-blur-sm"
-            >
-              <div className="modal-container transition-transform transform hover:scale-110">
-                <div className=" bg-white rounded-lg shadow-xl p-8">
-                  <div className=" mb-4 flex justify-between">
-                    <h2 className=" text-2xl font-semibold my-auto">
-                      Requested From {"Naufal Zaki Kemana"}
-                    </h2>
-                    <button
-                      onClick={toggleModal}
-                      className=" my-auto focus:outline-none transition-transform transform hover:scale-110"
-                    >
-                      <AiFillCloseCircle className="text-4xl" />
-                    </button>
-                  </div>
-
-                  <div className="mb-4">
-                    <label className="text-gray-600 font-semibold">
-                      Descriotion :
-                    </label>
-                    <p>
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                      Recusandae delectus sapiente cumque <br />
-                      corporis deleniti vero culpa perspiciatis animi
-                      reprehenderit repellat voluptas voluptate voluptates,{" "}
-                      <br />
-                      quis nobis nulla, consectetur repudiandae assumenda
-                      laudantium."{" "}
-                    </p>
-                  </div>
-                  <div className="mb-9">
-                    <label className="text-gray-600 font-semibold">
-                      Dokumen File :
-                    </label>
-                    <p>https://git-scm.com/download/win</p>
-                  </div>
-
-                  <div className=" flex flex-row gap-4 w-full align-bottom  justify-center">
-                    <button className="  hover:bg-secondaryAlternative transition-transform transform hover:scale-105 active:scale-95 p-4  bg-secondary  w-32  rounded-md  text-white">
-                      Approve
-                    </button>
-                    <button className="  hover:bg-primaryAlternative transition-transform transform hover:scale-105 active:scale-95 p-4 bg-primary w-32  rounded-md text-white">
-                      Decline
-                    </button>
-                  </div>
-                </div>
-              </div>
-              s
-            </div>
-          )}
-        </div>
-        <div className=" w-full flex flex-col ">
-          <div className="w-full shadow-xl flex flex-col  rounded-lg  duration-300 bg-silver p-4">
-            <div className="flex flex-col md:flex-row justify-center w-full px-4">
-              <div>
-                <img
-                  className="rounded-full h-36 w-36 my-auto bg-black mx-auto mb-4 md:mb-0"
-                  src={""}
-                  alt="/"
-                />
-              </div>
-
-              <div className="flex flex-auto flex-col my-auto w-full gap-1 sm:text-left mb-4">
-                <label className="font-bold mx-auto md:mx-8 text-lg md:text-2xl mb-2">
-                  Naufal Zaki Kemana
-                </label>
-                <label className="mx-auto md:mx-8 text-md md:text-lg">
-                  <span className="font-semibold text-md md:text-lg ">
-                    Project :{" "}
                   </span>
-                  <span className=" text-blue-800 text-md md:text-lg">
-                    TPLM
-                  </span>
-                </label>
-                <label className="mx-auto md:mx-8 text-md md:text-lg">
-                  <span className="font-semibold text-md md:text-lg">
-                    Team :
-                  </span>{" "}
-                  Webpro
-                </label>
-                <label className="mx-auto md:mx-8 text-md md:text-lg">
-                  <span className="font-semibold text-md md:text-lg">
-                    Message
-                  </span>{" "}
-                  : izin kan saya masuk
+                  : Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Possimus nisi eum voluptas ea error velit quia aperiam
+                  asperiores reiciendis ut quaerat sunt harum, delectus,
+                  accusantium molestias eligendi eos obcaecati. Repellendus!
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Voluptatibus architecto consequuntur, sunt aspernatur vel
+                  maxime accusamus eos magni recusandae adipisci, sed, quidem
+                  voluptatum aliquam officiis veniam esse placeat reiciendis
+                  quos.
                 </label>
               </div>
               <div className=" flex flex-row md:flex-col justify-center gap-4 my-2">
