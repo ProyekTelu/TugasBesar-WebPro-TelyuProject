b03b485b4369bab7b816176aaade32bb2eddc1de
RaihanSulthon <raihansy@student.telkomuniversity.ac.id>
2023-12-11 21:37:14 +0700

    test

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("telyu_project", "root", "root", {
+const db = new Sequelize("telyu_project", "root", "", {
   host: "localhost",
   dialect: "mysql",
 });
