8de1c38e5b54395ead18b37e5edd76603658b0c7
mzaky <mzakyf5503@gmail.com>
2024-05-21 19:02:25 +0700

    disable cors

--- Backend/index.js
+++ Backend/index.js
@@ -40,6 +40,16 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json());
 app.use(fileUpload());
 app.use(express.urlencoded({ extended: true }));
