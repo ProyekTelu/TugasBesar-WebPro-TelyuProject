631482e35272caac74f34b80bc392fc39b35d4fc
mzaky <mzakyf5503@gmail.com>
2024-05-21 19:11:11 +0700

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -37,18 +37,9 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://telu-project.vercel.app/",
   })
 );
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use(express.json());
 app.use(fileUpload());
