450f8234829fa685f0d15c7e1480c40702836812
mzaky <mzakyf5503@gmail.com>
2024-05-22 11:16:29 +0700

    add d

--- Backend/routes/MajorRoutes.js
+++ Backend/routes/MajorRoutes.js
@@ -3,7 +3,10 @@ import {
   getAllMajor,
   getMajorByFacultyCode,
 } from "../controllers/MajorController.js";
+
 const router = express.Router();
+
+export const maxDuration = 50;
 router.get("/major/:facultyCode", getMajorByFacultyCode);
 router.get("/major", getAllMajor);
 
